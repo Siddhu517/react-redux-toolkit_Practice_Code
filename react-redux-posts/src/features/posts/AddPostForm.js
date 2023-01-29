76ca4dfd0b2abec8051f3f40a05cf05d4188cd3f
Siddhu517 <siddhusadadekar517@gmail.com>
2023-01-29 21:54:46 +0530

    new change

--- react-redux-posts/src/features/posts/AddPostForm.js
+++ react-redux-posts/src/features/posts/AddPostForm.js
@@ -1,24 +1,37 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import { postAdded } from "./postsSlice";
+import { selectAllUsers } from "../users/usersSlice";
 
 const AddPostForm = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const [userId, setUserId] = useState("");
+
+  const users = useSelector(selectAllUsers);
 
   const onTitleChanged = (e) => setTitle(e.target.value);
   const onContentChanged = (e) => setContent(e.target.value);
+  const onAuthorChanged = (e) => setUserId(e.target.value);
 
   const dispatch = useDispatch();
 
   const onSavePostClicked = () => {
     if (title && content) {
-      dispatch(postAdded(title, content));
+      dispatch(postAdded(title, content, userId));
       setTitle("");
       setContent("");
     }
   };
+
+  const canSave = Boolean(title) && Boolean(content) && Boolean(userId);
+
+  const usersOptions = users.map((user) => (
+    <option key={user.id} value={user.id}>
+      {user.name}
+    </option>
+  ));
 
   return (
     <section className="d-flex justify-content-center align-items-center flex-column mt-4">
@@ -35,6 +48,15 @@ const AddPostForm = () => {
           className="form-control"
           onChange={onTitleChanged}
         />
+
+        <label htmlFor="postAuthor" className="form-label">
+          Author:
+        </label>
+        <select id="postAuthor" className="form-control" value={userId} onChange={onAuthorChanged}>
+          <option value=""></option>
+          {usersOptions}
+        </select>
+
         <label htmlFor="postTitle" className="form-label">
           Content:
         </label>
@@ -49,6 +71,7 @@ const AddPostForm = () => {
           type="button"
           onClick={onSavePostClicked}
           className="btn btn-primary rounded-pill mt-4 mb-5"
+          disabled={!canSave}
         >
           Save Post
         </button>
--- react-redux-posts/src/features/posts/PostsList.js
+++ react-redux-posts/src/features/posts/PostsList.js
@@ -2,13 +2,34 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { selectAllPosts } from "./postsSlice";
 
+import PostAuthor from "./PostAuthor";
+
+import TimeAgo from "./TimeAgo";
+
+import ReactionButtons from "./ReactionButtons";
+
 const PostsList = () => {
   const posts = useSelector(selectAllPosts);
 
-  const renderPosts = posts.map((post) => (
-    <article id={post.id} className="card w-auto h-auto p-5 m-4 bg-black text-light bg-opacity-75" >
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date));
+
+  const renderPosts = orderedPosts.map((post) => (
+    <article
+      id={post.id}
+      key={post.id}
+      className="d-flex justify-content-center align-items-start card w-auto h-auto p-5 m-4 bg-black text-light bg-opacity-75"
+    >
       <h3 className="mb-3">{post.title}</h3>
       <p>{post.content.substring(0, 100)}</p>
+
+      <p className="postCredit">
+        <PostAuthor userId={post.user} />
+        <TimeAgo timestamp={post.date} />
+      </p>
+
+      <ReactionButtons post={post} />
     </article>
   ));
 
